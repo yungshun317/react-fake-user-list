32c4b57546e45bc649f8389fd680d78abd97aa78
yungshun317 <seraph317@gmail.com>
2021-06-07 11:50:51 +0800

    [FEAT] Create a large array of fake data using `faker`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from "react";
+import faker from "faker";
+
+const bigList = [...Array(5000)].map(() => ({
+	name: faker.name.findName(),
+	email: faker.internet.email(),
+	avatar: faker.internet.avatar()
+}));
+
+function List({ data = [], renderItem, renderEmpty }) {
+	return !data.length ? (
+		renderEmpty
+	) : (
+	    <ul>
+	        {data.map((item, i) => (
+	        	<li key={i}>{renderItem(item)}</li>
+	        ))}
+	    </ul>
+	);
+}
 
 export default function App() {
-	return <h2>This is App.</h2>
+    const renderItem = item => (
+    	<div style={{ display: "flex" }}>
+    	    <img src={item.avatar} alt={item.name} width={50} />
+    	    <p>
+    	        {item.name} - {item.email}
+    	    </p>
+    	</div>
+    )
+
+	return <List data={bigList} renderItem={renderItem} />;
 }
